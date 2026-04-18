97ae4e5f212c3a5f8baf18c1eeb2fd3c42bebb89
nicole926 <ananicolegomesmendes@gmail.com>
2026-04-18 17:42:39 -0300

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var tarefa = document.getElementById('tarefa'); // input para digitar a tarefa
+var adicionar = document.getElementById('adicionar'); // botão de adicionar
+var lista = document.getElementById('lista'); // ul
+
